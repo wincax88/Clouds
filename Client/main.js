590db90b214849c4cd718be796d423dc2940849d
Highness Dark <Highness Dark>
2016-06-25 22:23:03 +0800

    Signed-off-by: Highness Dark <Highness Dark>

--- Client/main.js
+++ Client/main.js
@@ -11,7 +11,6 @@ function createWindow() {
 
     mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
-    // and load the index.html of the app.
     mainWindow.loadURL(`file://${__dirname}/pages/index.html`)
 
 
